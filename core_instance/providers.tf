cabf66c295b185ee3e282aa5a4117de2a0cdf6b7
Cuan Leo <cuanleo@gmail.com>
2023-05-06 17:50:12 +0100

    remove tls and local providers for core instance

--- core_instance/providers.tf
+++ core_instance/providers.tf
@@ -4,14 +4,6 @@ terraform {
       source  = "oracle/oci"
       version = "4.107.0"
     }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "4.0.4"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "2.4.0"
-    }
   }
   backend "remote" {}
 }
