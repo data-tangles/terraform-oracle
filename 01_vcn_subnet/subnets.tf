89abee72ca0b1abd803091e97cca809310c6f4ee
qman-being <cuanleo@gmail.com>
2023-05-07 18:47:37 +0100

    add subnet association to route table

--- 01_vcn_subnet/subnets.tf
+++ 01_vcn_subnet/subnets.tf
@@ -3,6 +3,7 @@ resource "oci_core_subnet" "vm_subnet" {
   cidr_block     = var.vm_subnet_cidr_block
   compartment_id = var.compartment_id
   vcn_id         = oci_core_vcn.main_vcn.id
+  route_table_id = oci_core_route_table.main_route_table
 
   freeform_tags     = merge(local.common_tags)
   display_name      = var.vm_subnet_display_name
