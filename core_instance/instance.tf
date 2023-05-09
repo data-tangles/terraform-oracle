a45a0d627f351102bdcc6ea8c79e3dd082850360
Cuan Leo <cuanleo@gmail.com>
2023-05-09 19:23:23 +0100

    add config for k3s instances

--- core_instance/instance.tf
+++ core_instance/instance.tf
@@ -1,3 +1,5 @@
+# Docker Node
+
 resource "oci_core_instance" "linux_instance" {
   availability_domain = var.availability_domain
   compartment_id      = var.compartment_id
@@ -6,14 +8,79 @@ resource "oci_core_instance" "linux_instance" {
 
   create_vnic_details {
     subnet_id        = var.subnet_id
+    private_ip       = var.docker_private_ip
     display_name     = "${var.instance_display_name}-nic"
-    assign_public_ip = true
+    assign_public_ip = false
     freeform_tags    = merge(local.common_tags)
   }
 
   source_details {
     source_type = "image"
     source_id   = var.image_source_id
+  }
+
+  metadata = {
+    ssh_authorized_keys = file(var.ssh_public_key)
+  }
+
+}
+
+# k3s Node 1
+
+resource "oci_core_instance" "linux_instance_k3s_1" {
+  availability_domain = var.k3s_availability_domain
+  compartment_id      = var.compartment_id
+  shape               = var.k3s_shape
+  display_name        = var.k3s_1_instance_display_name
+
+  create_vnic_details {
+    subnet_id        = var.subnet_id
+    private_ip       = var.k3s_1_private_ip
+    display_name     = "${var.k3s_1_instance_display_name}-nic"
+    assign_public_ip = false
+    freeform_tags    = merge(local.common_tags)
+  }
+
+  source_details {
+    source_type = "image"
+    source_id   = var.image_source_id
+  }
+
+  shape_config {
+    memory_in_gbs = var.instance_shape_config_memory_in_gbs
+    ocpus         = var.instance_shape_config_ocpus
+  }
+
+  metadata = {
+    ssh_authorized_keys = file(var.ssh_public_key)
+  }
+
+}
+
+# k3s Node 2
+
+resource "oci_core_instance" "linux_instance_k3s_2" {
+  availability_domain = var.k3s_availability_domain
+  compartment_id      = var.compartment_id
+  shape               = var.k3s_shape
+  display_name        = var.k3s_2_instance_display_name
+
+  create_vnic_details {
+    subnet_id        = var.subnet_id
+    private_ip       = var.k3s_2_private_ip
+    display_name     = "${var.k3s_2_instance_display_name}-nic"
+    assign_public_ip = false
+    freeform_tags    = merge(local.common_tags)
+  }
+
+  source_details {
+    source_type = "image"
+    source_id   = var.image_source_id
+  }
+
+  shape_config {
+    memory_in_gbs = var.instance_shape_config_memory_in_gbs
+    ocpus         = var.instance_shape_config_ocpus
   }
 
   metadata = {
--- core_instance/variables.tf
+++ core_instance/variables.tf
@@ -68,5 +68,57 @@ variable "ssh_public_key" {
   description = "SSH Public Key for auth to Core Instance"
 }
 
+variable "k3s_shape" {
+  type        = string
+  description = "Shape of the k3s Core Instance"
+}
+
+variable "k3s_availability_domain" {
+  type        = string
+  description = "Availability Domain for the k3s Core Instance"
+}
+
+variable "instance_shape_config_memory_in_gbs" {
+  type        = string
+  description = "Memory for core instance"
+}
+
+variable "instance_shape_config_ocpus" {
+  type        = string
+  description = "CPU for core instance"
+}
+
+variable "docker_private_ip" {
+  type        = string
+  description = "Private IP for Docker instance"
+}
+
+variable "k3s_1_instance_display_name" {
+  type        = string
+  description = "Display name for k3s instance 1"
+}
+
+variable "k3s_2_instance_display_name" {
+  type        = string
+  description = "Display name for k3s instance 2"
+}
+
+variable "k3s_1_private_ip" {
+  type        = string
+  description = "Private IP for k3s instance 1"
+}
+
+variable "k3s_2_private_ip" {
+  type        = string
+  description = "Private IP for k3s instance 2"
+}
+
+
+
+
+
+
+
+
 
 
