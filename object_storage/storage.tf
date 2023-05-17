2aa80f1e996fe16e758206818f7dde97c9d20b4a
qman-being <cuanleo@gmail.com>
2023-05-17 10:07:45 +0100

    add object storage module

---
+++ object_storage/storage.tf
@@ -0,0 +1,6 @@
+resource "oci_objectstorage_bucket" "oci_k3s_bucket" {
+    compartment_id = var.compartment_id
+    name = var.bucket_name
+    namespace = var.bucket_namespace
+    freeform_tags    = merge(local.common_tags)
+}
